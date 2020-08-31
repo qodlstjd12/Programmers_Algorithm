ea9af72ea65540d81316038a9457a694b62da787
qodlstjd12 <duddl6701@naver.com>
2020-08-31 19:11:18 +0900

    방금 커밋한거에서 i = j를 for문 안쪽으로 넣어서 코드를 정리했습니다.

--- Project1/Project1/같은수는싫어.cpp
+++ Project1/Project1/같은수는싫어.cpp
@@ -21,17 +21,16 @@ vector<int> solution(vector<int> arr)
 {
 	vector<int> answer;
 	int i, j;
-	for (i = 0; i < arr.size();) {
+	for (i = 0; i < arr.size(); i = j) {
 		answer.push_back(arr[i]);
 		for (j = i + 1; j < arr.size() && arr[i] == arr[j]; j++);
-		i = j;
 	}
 	return answer;
 }
 
 int main()
 {
-	vector<int> arr{ 4,4,4,4,4,4,4,3,3,3,3,3,4,4,3,4,3,3};
+	vector<int> arr{ 1,1,3,3,0,1,1};
 	
 	vector<int> temp = solution(arr);
 
